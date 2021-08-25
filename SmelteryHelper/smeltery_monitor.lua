cae790e9118ba193fde64ca8b385cf2f14aaa89a
Canadiia <ZachLDuncan@gmail.com>
2021-08-25 13:29:15 -0400

    better globals

--- SmelteryHelper/smeltery_monitor.lua
+++ SmelteryHelper/smeltery_monitor.lua
@@ -7,222 +7,6 @@ This programe will be used to read a Tinkers'
 	VSCode max col = 52
 ]]
 
-Monitor = peripheral.wrap("monitor_2")
-Graphics_Mode = false
-Offset = 1
-Ore_Colors = {
-	clay = {
-		bar_color = colors.brown,
-		text_color = colors.white,
-		name = "Clay",
-		bar_name = "balls"
-	},
-	glass = {
-		bar_color = colors.lightBlue,
-		text_color = colors.black,
-		name = "Glass",
-		bar_name = "blocks"
-	},
-	obsidian = {
-		bar_color = colors.gray,
-		text_color = colors.white,
-		name = "blocks",
-		bar_name = ""
-	},
-	ender = {
-		bar_color = colors.green,
-		text_color = colors.white,
-		name = "Ender",
-		bar_name = "pearls"
-	},
-	emerald = {
-		bar_color = colors.lime,
-		text_color = colors.black,
-		name = "Emerald",
-		bar_name = "gems"
-	},
-	quartz = {
-		bar_color = colors.lightGray,
-		text_color = colors.black,
-		name = "Quartz",
-		bar_name = "shards"
-	},
-	diamond = {
-		bar_color = colors.cyan,
-		text_color = colors.white,
-		name = "Diamond",
-		bar_name = "gems"
-	},
-	debris = {
-		bar_color = colors.brown,
-		text_color = colors.white,
-		name = "Ancient Debris",
-		bar_name = "shards"
-	},
-	iron = {
-		bar_color = colors.red,
-		text_color = colors.black,
-		name = "Iron",
-		bar_name = "bars"
-	},
-	gold = {
-		bar_color = colors.yellow,
-		text_color = colors.black,
-		name = "Butter",
-		bar_name = "bars"
-	},
-	copper = {
-		bar_color = colors.orange,
-		text_color = colors.black,
-		name = "Copper",
-		bar_name = "bars"
-	},
-	cobalt = {
-		bar_color = colors.blue,
-		text_color = colors.white,
-		name = "Cobalt",
-		bar_name = "bars"
-	},
-	slime_steel = {
-		bar_color = colors.lightBlue,
-		text_color = colors.black,
-		name = "Slime Steel",
-		bar_name = "bars"
-	},
-	tinkers_bronze = {
-		bar_color = colors.orange,
-		text_color = colors.black,
-		name = "Tinker's Bronze",
-		bar_name = "bars"
-	},
-	rose_gold = {
-		bar_color = colors.pink,
-		text_color = colors.black,
-		name = "Rose Gold",
-		bar_name = "bars"
-	},
-	pig_iron = {
-		bar_color = colors.magenta,
-		text_color = colors.black,
-		name = "Pig Iron",
-		bar_name = "bars"
-	},
-	manyullyn = {
-		bar_color = colors.purple,
-		text_color = colors.white,
-		name = "Manyullyn",
-		bar_name = "bars"
-	},
-	hepatizon = {
-		bar_color = colors.purple,
-		text_color = colors.white,
-		name = "Hepatitis",
-		bar_name = "bars"
-	},
-	queens_slime = {
-		bar_color = colors.green,
-		text_color = colors.white,
-		name = "Queen's Slime",
-		bar_name = "bars"
-	},
-	netherite = {
-		bar_color = colors.gray,
-		text_color = colors.white,
-		name = "Netherite",
-		bar_name = "bars"
-	},
-	tin = {
-		bar_color = colors.lightGray,
-		text_color = colors.black,
-		name = "Tin",
-		bar_name = "bars"
-	},
-	aluminum = {
-		bar_color = colors.lightGray,
-		text_color = colors.black,
-		name = "Aluminum",
-		bar_name = "bars"
-	},
-	lead = {
-		bar_color = colors.gray,
-		text_color = colors.white,
-		name = "Lead",
-		bar_name = "bars"
-	},
-	silver = {
-		bar_color = colors.lightGray,
-		text_color = colors.black,
-		name = "Silver",
-		bar_name = "bars"
-	},
-	nickel = {
-		bar_color = colors.yellow,
-		text_color = colors.black,
-		name = "Nickel",
-		bar_name = "bars"
-	},
-	zinc = {
-		bar_color = colors.lightGray,
-		text_color = colors.black,
-		name = "Zinc",
-		bar_name = "bars"
-	},
-	osmium = {
-		bar_color = colors.lightBlue,
-		text_color = colors.black,
-		name = "Osmium",
-		bar_name = "bars"
-	},
-	uranium = {
-		bar_color = colors.green,
-		text_color = colors.white,
-		name = "Uranium",
-		bar_name = "bars"
-	},
-	bronze = {
-		bar_color = colors.orange,
-		text_color = colors.black,
-		name = "Bronze",
-		bar_name = "bars"
-	},
-	brass = {
-		bar_color = colors.yellow,
-		text_color = colors.black,
-		name = "Brass",
-		bar_name = "bars"
-	},
-	electrum = {
-		bar_color = colors.yellow,
-		text_color = colors.black,
-		name = "Electrum",
-		bar_name = "bars"
-	},
-	invar = {
-		bar_color = colors.lightGray,
-		text_color = colors.black,
-		name = "Invar",
-		bar_name = "bars"
-	},
-	constantan = {
-		bar_color = colors.orange,
-		text_color = colors.black,
-		name = "Constantan",
-		bar_name = "bars"
-	},
-	pewter = {
-		bar_color = colors.yellow,
-		text_color = colors.black,
-		name = "Pewter",
-		bar_name = "bars"
-	},
-	steel = {
-		bar_color = colors.gray,
-		text_color = colors.white,
-		name = "Steel",
-		bar_name = "bars"
-	}
-}
-
 local function draw_box_from_center(
 	center_x,
 	center_y,
@@ -424,7 +208,6 @@ local function fill_gui_tank(
 			the GUI accurate.
 		]]
 
-		--TODO: Put ore text, centered, over spots in gui
 		local max_string_lenght = math.ceil(width / 2)
 
 		-- clear tank contents
@@ -537,13 +320,13 @@ local function read_smeltery()
 	-- wrap the key elements of the smeltery that
 	--  we need to read from.
 	local smeltery = peripheral.wrap(
-		"tconstruct:smeltery_0"
+		Peripherals.smeltery
 	)
 	local duct = peripheral.wrap(
-		"tconstruct:duct_0"
+		Peripherals.duct
 	)
 	local lava_tank = peripheral.wrap(
-		"tconstruct:tank_1"
+		Peripherals.tanks[1]
 	)
 
 	-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -556,42 +339,45 @@ local function read_smeltery()
 		quickly just comment or uncomment it and
 		it'll start reading live minecraft data.
 	]]
-	-- smeltery = {}
-	-- duct = {}
-	-- lava_tank = {}
-	-- smeltery.size = function()
-	-- 	return 18
-	-- end
+	if not INGAME then
+		smeltery = {}
+		duct = {}
+		lava_tank = {}
 
-	-- duct.tanks = function()
-	-- 	return {
-	-- 		[1] = {
-	-- 			amount = 5000,
-	-- 			name = "tconstruct:molten_iron"
-	-- 		},
-	-- 		[2] = {
-	-- 			amount = 2500,
-	-- 			name = "tconstruct:molten_debris"
-	-- 		},
-	-- 		[3] = {
-	-- 			amount = 15 * 144,
-	-- 			name = "tconstruct:molten_ender"
-	-- 		},
-	-- 		[4] = {
-	-- 			amount = 25 * 144,
-	-- 			name = "tconstruct:molter_netherite"
-	-- 		}
-	-- 	}
-	-- end
+		smeltery.size = function()
+			return 18
+		end
 
-	-- lava_tank.tanks = function()
-	-- 	return {
-	-- 		{
-	-- 			amount = 4000,
-	-- 			name = "minecraft:lava"
-	-- 		}
-	-- 	}
-	-- end
+		duct.tanks = function()
+			return {
+				[1] = {
+					amount = 5000,
+					name = "tconstruct:molten_iron"
+				},
+				[2] = {
+					amount = 2500,
+					name = "tconstruct:molten_debris"
+				},
+				[3] = {
+					amount = 15 * 144,
+					name = "tconstruct:molten_ender"
+				},
+				[4] = {
+					amount = 25 * 144,
+					name = "tconstruct:molter_netherite"
+				}
+			}
+		end
+
+		lava_tank.tanks = function()
+			return {
+				{
+					amount = 4000,
+					name = "minecraft:lava"
+				}
+			}
+		end
+	end
 	-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 	--[[
@@ -638,6 +424,8 @@ end
 
 local function print_smeltery_details(
 	width,
+	height,
+	tank_height,
 	current_fill_level,
 	ingot_capacity,
 	fluid_capacity,
@@ -649,6 +437,16 @@ local function print_smeltery_details(
 		information in a nice way on the left half of the
 		monitor.
 	]]
+
+		-- clear details contents
+		draw_box_from_center(
+			0.25 * width,
+			(height / 2) + 1,
+			(width / 2) - 1,
+			height - 1,
+			colors.black,
+			true
+		)
 
 	-- set the cursor just below the left side's title,
 	-- 	and make sure the background is black and the
@@ -674,7 +472,12 @@ local function print_smeltery_details(
 	-- 	ingot capacity.
 	local ingot_capacity_string =
 		"Ingot Capacity: "
-		.. string.format("%.2f", ingot_capacity - (current_fill_level * ingot_capacity))
+		.. string.format(
+			"%.2f",
+			ingot_capacity - (
+				current_fill_level * ingot_capacity
+			)
+		)
 		.. " free of "
 		.. ingot_capacity
 		.. " Total ingots."
@@ -689,7 +492,12 @@ local function print_smeltery_details(
 	elseif (
 		string.len(
 			"\t"
-			.. string.format("%.2f", ingot_capacity - (current_fill_level * ingot_capacity))
+			.. string.format(
+				"%.2f",
+				ingot_capacity - (
+					current_fill_level * ingot_capacity
+				)
+			)
 			.. " free of "
 			.. ingot_capacity
 			.. " Total ingots.\n"
@@ -697,7 +505,12 @@ local function print_smeltery_details(
 	) then
 		print(
 			"Ingot Capacity: \n\t"
-			.. string.format("%.2f", ingot_capacity - (current_fill_level * ingot_capacity))
+			.. string.format(
+				"%.2f",
+				ingot_capacity - (
+					current_fill_level * ingot_capacity
+				)
+			)
 			.. " free of "
 			.. ingot_capacity
 			.. " Total ingots.\n"
@@ -705,7 +518,12 @@ local function print_smeltery_details(
 	else
 		print(
 			"Ingot Capacity: \n\t"
-			.. string.format("%.2f", ingot_capacity - (current_fill_level * ingot_capacity))
+			.. string.format(
+				"%.2f",
+				ingot_capacity - (
+					current_fill_level * ingot_capacity
+				)
+			)
 			.. " free of "
 			.. ingot_capacity
 			.. " max.\n"
@@ -765,11 +583,314 @@ local function print_smeltery_details(
 	end
 end
 
+local function initialize_globals()
+	Graphics_Mode = false
+	Offset = 1
+	Ore_Colors = {
+		clay = {
+			bar_color = colors.brown,
+			text_color = colors.white,
+			name = "Clay",
+			bar_name = "balls"
+		},
+		glass = {
+			bar_color = colors.lightBlue,
+			text_color = colors.black,
+			name = "Glass",
+			bar_name = "blocks"
+		},
+		obsidian = {
+			bar_color = colors.gray,
+			text_color = colors.white,
+			name = "blocks",
+			bar_name = ""
+		},
+		ender = {
+			bar_color = colors.green,
+			text_color = colors.white,
+			name = "Ender",
+			bar_name = "pearls"
+		},
+		emerald = {
+			bar_color = colors.lime,
+			text_color = colors.black,
+			name = "Emerald",
+			bar_name = "gems"
+		},
+		quartz = {
+			bar_color = colors.lightGray,
+			text_color = colors.black,
+			name = "Quartz",
+			bar_name = "shards"
+		},
+		diamond = {
+			bar_color = colors.cyan,
+			text_color = colors.white,
+			name = "Diamond",
+			bar_name = "gems"
+		},
+		debris = {
+			bar_color = colors.brown,
+			text_color = colors.white,
+			name = "Ancient Debris",
+			bar_name = "shards"
+		},
+		iron = {
+			bar_color = colors.red,
+			text_color = colors.black,
+			name = "Iron",
+			bar_name = "bars"
+		},
+		gold = {
+			bar_color = colors.yellow,
+			text_color = colors.black,
+			name = "Butter",
+			bar_name = "bars"
+		},
+		copper = {
+			bar_color = colors.orange,
+			text_color = colors.black,
+			name = "Copper",
+			bar_name = "bars"
+		},
+		cobalt = {
+			bar_color = colors.blue,
+			text_color = colors.white,
+			name = "Cobalt",
+			bar_name = "bars"
+		},
+		slime_steel = {
+			bar_color = colors.lightBlue,
+			text_color = colors.black,
+			name = "Slime Steel",
+			bar_name = "bars"
+		},
+		tinkers_bronze = {
+			bar_color = colors.orange,
+			text_color = colors.black,
+			name = "Tinker's Bronze",
+			bar_name = "bars"
+		},
+		rose_gold = {
+			bar_color = colors.pink,
+			text_color = colors.black,
+			name = "Rose Gold",
+			bar_name = "bars"
+		},
+		pig_iron = {
+			bar_color = colors.magenta,
+			text_color = colors.black,
+			name = "Pig Iron",
+			bar_name = "bars"
+		},
+		manyullyn = {
+			bar_color = colors.purple,
+			text_color = colors.white,
+			name = "Manyullyn",
+			bar_name = "bars"
+		},
+		hepatizon = {
+			bar_color = colors.purple,
+			text_color = colors.white,
+			name = "Hepatitis",
+			bar_name = "bars"
+		},
+		queens_slime = {
+			bar_color = colors.green,
+			text_color = colors.white,
+			name = "Queen's Slime",
+			bar_name = "bars"
+		},
+		netherite = {
+			bar_color = colors.gray,
+			text_color = colors.white,
+			name = "Netherite",
+			bar_name = "bars"
+		},
+		tin = {
+			bar_color = colors.lightGray,
+			text_color = colors.black,
+			name = "Tin",
+			bar_name = "bars"
+		},
+		aluminum = {
+			bar_color = colors.lightGray,
+			text_color = colors.black,
+			name = "Aluminum",
+			bar_name = "bars"
+		},
+		lead = {
+			bar_color = colors.gray,
+			text_color = colors.white,
+			name = "Lead",
+			bar_name = "bars"
+		},
+		silver = {
+			bar_color = colors.lightGray,
+			text_color = colors.black,
+			name = "Silver",
+			bar_name = "bars"
+		},
+		nickel = {
+			bar_color = colors.yellow,
+			text_color = colors.black,
+			name = "Nickel",
+			bar_name = "bars"
+		},
+		zinc = {
+			bar_color = colors.lightGray,
+			text_color = colors.black,
+			name = "Zinc",
+			bar_name = "bars"
+		},
+		osmium = {
+			bar_color = colors.lightBlue,
+			text_color = colors.black,
+			name = "Osmium",
+			bar_name = "bars"
+		},
+		uranium = {
+			bar_color = colors.green,
+			text_color = colors.white,
+			name = "Uranium",
+			bar_name = "bars"
+		},
+		bronze = {
+			bar_color = colors.orange,
+			text_color = colors.black,
+			name = "Bronze",
+			bar_name = "bars"
+		},
+		brass = {
+			bar_color = colors.yellow,
+			text_color = colors.black,
+			name = "Brass",
+			bar_name = "bars"
+		},
+		electrum = {
+			bar_color = colors.yellow,
+			text_color = colors.black,
+			name = "Electrum",
+			bar_name = "bars"
+		},
+		invar = {
+			bar_color = colors.lightGray,
+			text_color = colors.black,
+			name = "Invar",
+			bar_name = "bars"
+		},
+		constantan = {
+			bar_color = colors.orange,
+			text_color = colors.black,
+			name = "Constantan",
+			bar_name = "bars"
+		},
+		pewter = {
+			bar_color = colors.yellow,
+			text_color = colors.black,
+			name = "Pewter",
+			bar_name = "bars"
+		},
+		steel = {
+			bar_color = colors.gray,
+			text_color = colors.white,
+			name = "Steel",
+			bar_name = "bars"
+		}
+	}
+	Peripherals = {
+        tanks = {}
+    }
+
+	if pcall(term.setGraphicsMode, true) then
+		term.setGraphicsMode(false)
+		INGAME = false
+	else
+		INGAME = true
+	end
+
+    local pulled_periphs = {}
+	if INGAME then
+		pulled_periphs = peripheral.getNames()
+	else
+		pulled_periphs = {
+			"back",
+			"monitor_5",
+			"tconstruct:smeltery_4",
+			"tconstruct:tank_1",
+			"monitor_3",
+			"tconstruct:basin_1",
+			"tconstruct:table_3",
+			"tconstruct:duct_0",
+            "left"
+		}
+	end
+
+	local monitors = {}
+	for _, value in ipairs(pulled_periphs) do
+		if string.find(value, "tconstruct:") then
+			local smeltery_periphs = string.sub(value, 12)
+			if (
+				string.find(smeltery_periphs, "smeltery")
+				or string.find(smeltery_periphs, "tank")
+				or string.find(smeltery_periphs, "duct")
+			) then
+				local _, name_end = string.find(smeltery_periphs, "_")
+                if string.sub(smeltery_periphs, 0, name_end - 1) == "tank" then
+                    table.insert(Peripherals.tanks, value)
+                else
+                    Peripherals[string.sub(smeltery_periphs, 0, name_end - 1)] = value
+                end
+			end
+		elseif (
+            string.find(value, "monitor")
+            or string.find(value, "left")
+            and peripheral.getType("left") == "monitor"
+            or string.find(value, "right")
+            and peripheral.getType("right") == "monitor"
+            or string.find(value, "up")
+            and peripheral.getType("up") == "monitor"
+            or string.find(value, "down")
+            and peripheral.getType("down") == "monitor"
+            or string.find(value, "back")
+            and peripheral.getType("back") == "monitor"
+            or string.find(value, "front")
+            and peripheral.getType("front") == "monitor"
+        ) then
+			table.insert(monitors, value)
+		end
+	end
+
+	if table.getn(monitors) > 1 then
+		local monitor_count = table.getn(monitors)
+		print(
+			"It looks like there are "
+			.. table.getn(monitors)
+			.. " monitors found.\n"
+			.. "From the list of monitors below, "
+			.. "which is the one you'd like to display "
+			.. "the smeltery information on?"
+		)
+		for index, monitor in ipairs(monitors) do
+			print(index .. ". " .. monitor)
+		end
+
+		local answer = read()
+	    if tonumber(answer) and tonumber(answer) <= monitor_count then
+			Monitor = peripheral.wrap(monitors[tonumber(answer)])
+		else
+			error("Invalid Monitor Selection")
+		end
+	end
+end
+
 local function main()
 	--[[
 	the main function where evrything is done and where
 		the main loop is.
 	]]
+
+	initialize_globals()
 
 	-- set the scale of the text on the monitor.
 	Monitor.setTextScale(0.75)
@@ -803,6 +924,8 @@ local function main()
 		-- print the current details of the smeltery.
 		print_smeltery_details(
 			width,
+			height,
+			tank_height,
 			current_fill_level,
 			read_data["max_ingots"],
 			read_data["max_fluids"],
